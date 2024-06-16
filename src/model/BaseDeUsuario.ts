98e2614b5e7627049d7dddcbf20ff212a8a9ae40
Vitor Lacerda <floco.vitor@gmail.com>
2024-06-16 18:50:46 -0300

    Inserção do método de salvar lista de usuarios.

--- src/model/BaseDeUsuario.ts
+++ src/model/BaseDeUsuario.ts
@@ -30,27 +30,34 @@ export default class BaseDeUsuario {
      * Obtém a lista de usuários do arquivo utilizando o método lerArquivo() de LerUsuario.
      * @returns Um array contendo todos os usuários lidos do arquivo.
      */
-    private getListaUsuarios(): Array<Usuario> {
+    public getListaUsuarios(): Array<Usuario> {
         return this.listaUsuarios;
     }
 
     /**
-     * Método privado que cria a lista de usuários utilizando o método lerArquivo() de LerUsuario.
+     * Método que cria a lista de usuários utilizando o método lerArquivo() de LerUsuario.
      */
-    private criaListaUsuario() {
+    public criaListaUsuario() {
         this.listaUsuarios = this.lerUsuario.lerArquivo();
     }
 
     /**
-     * Método privado que adiciona um novo usuário à lista de usuários.
+     * Método que adiciona um novo usuário à lista de usuários.
      * @param username Username do novo usuário.
      * @param nome Nome do novo usuário.
      * @param senha Senha do novo usuário.
      * @param idade Idade do novo usuário.
      * @param email Email do novo usuário.
      */
-    private adicionaUsuario(username: string, nome: string, senha: string, idade: number, email: string) {
+    public adicionaUsuario(username: string, nome: string, senha: string, idade: number, email: string) {
         let usuario = new Usuario(username, nome, senha, idade, email);
         this.listaUsuarios.push(usuario);
+    }
+
+    /**
+     * Método que salva a lista de usuários utilizando o método escreverArquivo() de LerUsuario.
+     */
+    public salvaListaUsuario() {
+        this.lerUsuario.escreverArquivo(this.getListaUsuarios);
     }
 }
