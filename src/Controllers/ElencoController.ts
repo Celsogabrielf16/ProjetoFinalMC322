cd96ca10cda84e545defb1f5f82d11af2be684b9
Vitor Lacerda <floco.vitor@gmail.com>
2024-06-17 13:30:15 -0300

    Implementação da classe ElencoController.

--- src/Controllers/ElencoController.ts
+++ src/Controllers/ElencoController.ts
@@ -3,9 +3,20 @@ class ElencoController {
     private elenco: Elenco;
     private elencoView: ElencoView;
 
+    /**
+     * Construtor da classe ElencoController.
+     * @param elencoRecebido O elenco a ser controlado por este controlador.
+     */
     constructor(elencoRecebido: Elenco) {
         this.elenco = elencoRecebido;
         this.elencoView = new ElencoView();
     }
 
+    /**
+     * Lista os atores do elenco e os exibe na view associada.
+     */
+    public listarAtores(): void {
+        let listaDeAtores: Array<Ator> = this.elenco.listarAtores();
+        this.elencoView.listarAtores(listaDeAtores);
+    }
 }
