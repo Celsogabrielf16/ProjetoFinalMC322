8c8d97c435497a5a873ae0c266b8eb194722f288
Vitor Lacerda <floco.vitor@gmail.com>
2024-06-17 13:33:39 -0300

    Implementação da classe AvaliacaoController.

--- src/Controllers/AvaliacaoController.ts
+++ src/Controllers/AvaliacaoController.ts
@@ -1,11 +1,54 @@
 class AvaliacaoController {
     
     private avaliacao: Avaliacao;
-    private avalicaoView: AvalicaoView;
+    private avaliacaoView: AvaliacaoView;
 
+    /**
+     * Construtor da classe AvaliacaoController.
+     * @param avaliacaoRecebida A avaliação a ser controlada por este controlador.
+     */
     constructor(avaliacaoRecebida: Avaliacao) {
         this.avaliacao = avaliacaoRecebida;
-        this.avalicaoView = new AvaliacaoView();
+        this.avaliacaoView = new AvaliacaoView();
     }
 
+    /**
+     * Obtém o ID da avaliação.
+     * @returns O ID da avaliação.
+     */
+    public getID(): number {
+        return this.avaliacao.getID();
+    }
+
+    /**
+     * Obtém a nota da avaliação.
+     * @returns A nota da avaliação.
+     */
+    public getAvaliacao(): number {
+        return this.avaliacao.getAvaliacao();
+    }
+
+    /**
+     * Obtém a data da avaliação.
+     * @returns A data da avaliação.
+     */
+    public getDataAvaliacao(): string {
+        return this.avaliacao.getDataAvaliacao();
+    }
+
+    /**
+     * Obtém o comentário da avaliação.
+     * @returns O comentário da avaliação.
+     */
+    public getComentario(): string {
+        return this.avaliacao.getComentario();
+    }
+
+    /**
+     * Obtém o usuário que realizou a avaliação.
+     * @returns O usuário que realizou a avaliação.
+     */
+    public getUsuario(): Usuario {
+        return this.avaliacao.getUsuario();
+    }
 }
