4c54681def89e8cb3896f2be47e4021631801051
Vitor Lacerda <floco.vitor@gmail.com>
2024-06-17 13:21:59 -0300

    Implementação da classe DiretorController.

--- src/Controllers/DiretorController.ts
+++ src/Controllers/DiretorController.ts
@@ -8,4 +8,27 @@ class DiretorController {
         this.diretorView = new DiretorView();
     }
 
+    /**
+     * Obtém o nome do diretor.
+     * @returns O nome do diretor.
+     */
+    public getNome(): string {
+        return this.diretor.getNome();
+    }
+
+    /**
+     * Obtém a idade do diretor.
+     * @returns A idade do diretor.
+     */
+    public getIdade(): number {
+        return this.diretor.getIdade();
+    }
+
+    /**
+     * Obtém informações adicionais sobre o diretor.
+     * @returns Informações adicionais sobre o diretor.
+     */
+    public getSobre(): string {
+        return this.diretor.getSobre();
+    }
 }
