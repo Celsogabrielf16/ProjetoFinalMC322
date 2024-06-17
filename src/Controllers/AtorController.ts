2f4eb7e4ad09dd82f80f702d91dc438639594439
Vitor Lacerda <floco.vitor@gmail.com>
2024-06-17 13:18:57 -0300

    Implementação da classe AtorController.

--- src/Controllers/AtorController.ts
+++ src/Controllers/AtorController.ts
@@ -3,9 +3,36 @@ class AtorController {
     private ator: Ator;
     private atorView: AtorView;
 
+    /**
+     * Construtor da classe AtorController.
+     * @param atorRecebido O ator a ser controlado por este controlador.
+     */
     constructor(atorRecebido: Ator) {
         this.ator = atorRecebido;
         this.atorView = new AtorView();
     }
 
+    /**
+     * Obtém o nome do ator.
+     * @returns O nome do ator.
+     */
+    public getNome(): string {
+        return this.ator.getNome();
+    }
+
+    /**
+     * Obtém a idade do ator.
+     * @returns A idade do ator.
+     */
+    public getIdade(): number {
+        return this.ator.getIdade();
+    }
+
+    /**
+     * Obtém o papel desempenhado pelo ator.
+     * @returns O papel desempenhado pelo ator.
+     */
+    public getPapel(): string {
+        return this.ator.getPapel();
+    }
 }
