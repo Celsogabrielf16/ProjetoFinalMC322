07abd4f222f170723465f8ca1cd255fa0f3fc33a
Vitor Lacerda <v231524@dac.unicamp.br>
2024-06-20 19:22:19 -0300

    Remoção de arquivo.

--- src/Controllers/test.ts
+++
@@ -1,5 +0,0 @@
-public cadastrarUsuario(username: string, nome: string, senha: string, idade: number, email: string): void {
-    const usuario = new Usuario(username, nome, senha, idade, email);
-    const listaUsuarios = BaseDeUsuario.getInstancia().getListaUsuarios();
-    listaUsuarios.push(usuario);
-}
