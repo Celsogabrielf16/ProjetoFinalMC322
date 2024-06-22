d2b8801d95ae4a970aac1008f43fb9053a6415a3
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-06-22 14:45:07 -0300

    Criação do arquivo HTML de todas as views da aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-console.log("Projeto Final MC322")
-
-import {LerArquivo} from "./model/LerArquivo";
-let midia = new LerArquivo();
-console.log(midia.lerArquivo());
-
+console.log("Projeto Final MC322");
