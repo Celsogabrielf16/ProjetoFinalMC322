8e2afbf4ea253a22c7cc05b7d54cd931c8d4a440
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-06-22 20:15:36 -0300

    Finalização do desenvolvimento da View Cadastrar

--- src/view/cadastrar/cadastrar.ts
+++ src/view/cadastrar/cadastrar.ts
@@ -1,1 +1,43 @@
-console.log("Cadastrar Works!");
+import BaseDeUsuarioController from "../../Controllers/BaseDeUsuarioController.js";
+
+class Cadastrar {
+    private baseDeUsuarioController: BaseDeUsuarioController;
+    private submitButton: HTMLElement;
+    private itemPassword: HTMLElement;
+    private nameInput: HTMLInputElement;
+    private emailInput: HTMLInputElement;
+    private senhaInput: HTMLInputElement;
+
+    constructor (baseDeUsuarioController: BaseDeUsuarioController) {
+        this.baseDeUsuarioController = baseDeUsuarioController;
+
+        this.submitButton = document.getElementById("submitButton") as HTMLElement;
+        this.itemPassword = document.getElementById("itemPassword") as HTMLElement;
+
+        this.nameInput = document.getElementById("name") as HTMLInputElement;
+        this.emailInput = document.getElementById("email") as HTMLInputElement;
+        this.senhaInput = document.getElementById("password") as HTMLInputElement;
+
+        this.addEventListeners();
+    }
+
+    private addEventListeners(): void {
+        this.submitButton.addEventListener('click', this.onClick.bind(this));
+    }
+
+    private onClick(): void {
+        this.itemPassword.classList.remove("form__item--error");
+        let autenticou = this.baseDeUsuarioController.cadastrarUsuario(this.nameInput.value, this.emailInput.value, this.senhaInput.value);
+
+        if(autenticou) {
+            window.location.href = `/src/view/home/home.html`;
+        } else {
+            this.itemPassword.classList.add("form__item--error");
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let baseDeUsuarioController: BaseDeUsuarioController = new BaseDeUsuarioController();
+    new Cadastrar(baseDeUsuarioController);
+});
