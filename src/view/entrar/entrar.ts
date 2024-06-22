f5c90faf6ec1f6b71d38334b6e712d10de7af6d0
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-06-22 18:45:55 -0300

    Finalização do desenvolvimento da submissão do formulario da View Entrar

--- src/view/entrar/entrar.ts
+++ src/view/entrar/entrar.ts
@@ -1,1 +1,31 @@
-console.log("Entrar Works!");
+import BaseDeUsuarioController from "../../Controllers/BaseDeUsuarioController.js";
+
+class Entrar {
+    private baseDeUsuarioController: BaseDeUsuarioController;
+    private submitButton: HTMLElement;
+    private emailInput: HTMLInputElement;
+    private senhaInput: HTMLInputElement;
+
+    constructor (baseDeUsuarioController: BaseDeUsuarioController) {
+        this.baseDeUsuarioController = baseDeUsuarioController;
+
+        this.submitButton = document.getElementById("submitButton") as HTMLElement;
+        this.emailInput = document.getElementById("email") as HTMLInputElement;
+        this.senhaInput = document.getElementById("password") as HTMLInputElement;
+
+        this.addEventListeners()
+    }
+
+    private addEventListeners(): void {
+        this.submitButton.addEventListener('click', this.onClick.bind(this));
+    }
+
+    private onClick(): void {
+        this.baseDeUsuarioController.autenticarUsuario(this.emailInput.value, this.senhaInput.value);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let baseDeUsuarioController: BaseDeUsuarioController = new BaseDeUsuarioController();
+    new Entrar(baseDeUsuarioController);
+});
