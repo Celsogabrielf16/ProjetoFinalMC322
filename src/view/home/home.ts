5c85f908523effdc7165fa4bc0d1d4d9e7599e7c
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-06-23 13:42:18 -0300

    Implementação do Slider na View Home

--- src/view/home/home.ts
+++ src/view/home/home.ts
@@ -1,11 +1,79 @@
 console.log("Home Works!");
 
-// @ts-ignore
-let slider = new KeenSlider("#my-keen-slider", {
-    loop: true,
-    mode: "free-snap",
-    slides: {
-        perView: 4.5,
-        spacing: 15,
-    },
-})
+fetch("/dist/assets/midia.json")
+    .then(res => res.json())
+    .then(listaMidias => {
+        const sliderFilmsElement: HTMLElement = document.getElementById("slider-films") as HTMLElement;
+        const sliderSeriesElement: HTMLElement = document.getElementById("slider-series") as HTMLElement;
+
+        console.log(listaMidias);
+        let contador = 0;
+        
+        if (sliderFilmsElement != null && sliderSeriesElement != null) {
+            listaMidias.forEach((midia: any) => {
+                sliderFilmsElement.innerHTML += `
+                <div class="keen-slider__slide">
+                    <a class="card__link" href="/${contador}">
+                        <img class="card__img" src="./../..${midia.imagemCapa}" alt="${midia.titulo}">
+                        <div class="card__informations">
+                            <h3 class="card__title">${midia.titulo}</h3>
+                            <div class="card__details">
+                                <div class="details__rating">
+                                    ${Math.floor(Math.random() * 5)}
+                                    <img src="/src/assets/icons/estrelaCheia.png" alt="">
+                                </div>
+                                <div class="details__separator"></div>
+                                <div>${midia.anoLancamento}</div>
+                                <div class="details__separator"></div>
+                                <div>${midia.faixaEtaria}</div>
+                            </div>
+                            <p class="card__synopsis">${midia.sinopse}</p>
+                        </div>
+                    </a>
+                </div>
+            `
+            sliderSeriesElement.innerHTML += `
+                <div class="keen-slider__slide">
+                    <a class="card__link" href="/${contador}">
+                        <img class="card__img" src="./../..${midia.imagemCapa}" alt="${midia.titulo}">
+                        <div class="card__informations">
+                            <h3 class="card__title">${midia.titulo}</h3>
+                            <div class="card__details">
+                                <div class="details__rating">
+                                    ${Math.floor(Math.random() * 5)}
+                                    <img src="/src/assets/icons/estrelaCheia.png" alt="">
+                                </div>
+                                <div class="details__separator"></div>
+                                <div>${midia.anoLancamento}</div>
+                                <div class="details__separator"></div>
+                                <div>${midia.faixaEtaria}</div>
+                            </div>
+                            <p class="card__synopsis">${midia.sinopse}</p>
+                        </div>
+                    </a>
+                </div>
+            `
+            contador++;
+            });
+        }
+
+        // @ts-ignore
+        let sliderFilms = new KeenSlider("#slider-films", {
+            loop: true,
+            mode: "free-snap",
+            dragSpeed: 0.3,
+            slides: {
+              perView: 5,
+            },
+          })
+        
+        // @ts-ignore
+        let sliderSeries = new KeenSlider("#slider-series", {
+            loop: true,
+            mode: "free-snap",
+            dragSpeed: 0.3,
+            slides: {
+                perView: 5,
+            },
+            })
+    })
