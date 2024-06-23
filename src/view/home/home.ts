5240553b711c77bfa9821ea99e56c5e85be1c0af
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-06-23 11:12:44 -0300

    Implementação da biblioteca Keen-Slider na View Home

--- src/view/home/home.ts
+++ src/view/home/home.ts
@@ -1,1 +1,11 @@
 console.log("Home Works!");
+
+// @ts-ignore
+let slider = new KeenSlider("#my-keen-slider", {
+    loop: true,
+    mode: "free-snap",
+    slides: {
+        perView: 4.5,
+        spacing: 15,
+    },
+})
